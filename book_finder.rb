32c25df1e9548c71a364c68ce877ac33cd86fe4a
christimrichmond <christimrichmond@gmail.com>
2018-01-19 09:14:45 -0500

    still have redirect issue

--- book_finder.rb
+++ book_finder.rb
@@ -1,9 +1,14 @@
 
-def book_name(title)
-   title_hash = {"Blink" =>"isbn_9780316010665", "Defying Hitler" =>"isbn_0312421133", "Keep the Home Fires Burning - Part One: Spitfire Down!" =>"isbn_1510059202", 
-    	"Ruby Cookbook: Recipes for Object-Oriented Scripting" =>"isbn_1449373712", "Test Driven Development in Ruby" =>"isbn_1484226372"}
+def book_name(isbn)
+   title_hash = {"Blink" =>"9780316010665", "Defying Hitler" =>"0312421133", "Keep the Home Fires Burning - Part One: Spitfire Down!" =>"1510059202", 
+    	"Ruby Cookbook: Recipes for Object-Oriented Scripting" =>"1449373712", "Test Driven Development in Ruby" =>"1484226372"}
     
-title_hash[title]
+title_hash[isbn]
 end
 
+def book_link(title)
+	title_link_hash = {"Blink" => "https://goo.gl/9riCRk", "Defying Hitler" => "https://goo.gl/gSFRkW", "Keep the Home Fires Burning - Part One: Spitfire Down!" => "https://goo.gl/JNXw46", "Ruby Cookbook: Recipes for Object-Oriented Scripting" => "https://goo.gl/MwPbXs", "Test Driven Development in Ruby" => "https://goo.gl/3JGfkp"}
+
+title_link_hash[title]
+end
 
--- book_finder_test.rb
+++ book_finder_test.rb
@@ -8,23 +8,23 @@ class Testbookfinder < Minitest::Test
 	end
 
 	def test_assert_Blink_returns_isbn_number
-		assert_equal("isbn_9780316010665", book_name("Blink"))
+		assert_equal("9780316010665", book_name("Blink"))
 	end
 
     def test_assert_Defying_Hitler_returns_isbn_number
-		assert_equal("isbn_0312421133", book_name("Defying Hitler"))
+		assert_equal("0312421133", book_name("Defying Hitler"))
 	end
 
     def test_assert_Keep_the_Home_Fires_Burning_returns_isbn_number
-		assert_equal("isbn_1510059202", book_name("Keep the Home Fires Burning - Part One: Spitfire Down!"))
+		assert_equal("1510059202", book_name("Keep the Home Fires Burning - Part One: Spitfire Down!"))
 	end
 
     def test_assert_Ruby_Cookbook_title_returns_isbn_number
-		assert_equal("isbn_1449373712", book_name("Ruby Cookbook: Recipes for Object-Oriented Scripting"))
+		assert_equal("1449373712", book_name("Ruby Cookbook: Recipes for Object-Oriented Scripting"))
 	end
 
     def test_assert_Test_Driven_Development_title_returns_isbn_number
-		assert_equal("isbn_1484226372", book_name("Test Driven Development in Ruby"))
+		assert_equal("1484226372", book_name("Test Driven Development in Ruby"))
 	end
 
  end
